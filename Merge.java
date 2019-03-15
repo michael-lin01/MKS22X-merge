41dcde65eb51fc10cad8a938c168a5bef8985db0
michaellin430 <michaellin430@gmail.com>
2019-03-15 16:46:28 -0400

    still an index out of bounds exception

--- Merge.java
+++ Merge.java
@@ -1,23 +1,26 @@
+import java.util.Arrays;
+
 public class Merge{
   public static void mergesort(int[] data){
     mergesort(data,0,data.length);
   }
 
   private static void mergesort(int[] data, int lo, int hi){
-    if(lo < hi){
+    if(lo < hi && data.length > 1){
       int[] left,right;
       left = new int[hi/2];
-      right = new int[data.length-hi/2];
-
+      right = new int[data.length-left.length];
       for(int i = 0; i<left.length;i++){
         left[i]=data[i];
       }
+      int n = 0;
       for(int i = left.length; i < data.length; i++){
-        right[i]=data[i];
+        System.out.println(""+i+" "+data[i]);
+        right[n]=data[i];
+        n++;
       }
-
-      mergesort(left,0,left.length);
-      mergesort(right,0,right.length);
+      mergesort(left,lo,left.length);
+      mergesort(right,left.length+1,hi);
       merge(left,right);
     }
   }
@@ -26,14 +29,25 @@ public class Merge{
     int[] out = new int[in1.length+in2.length];
     int current1 = 0;
     int current2 = 0;
+    
     for(int i = 0; i < in1.length+in2.length; i++){
-      if(in1[current1]<in2[current2]){
-        
+      System.out.println(current1);
+      System.out.println(current2);
+      if(current2>=in2.length||in1[current1]<in2[current2]){
+        out[i]=in1[current1];
+        current1++;
+      }
+      if(current1>=in1.length||in2[current2]<in1[current1]){
+        out[i]=in2[current2];
+        current2++;
       }
     }
   }
 
-
-
+  public static void main(String args[]){
+    int[] ary = {30,20,1};
+    mergesort(ary);
+    System.out.println(ary);
+  }
 
 }
